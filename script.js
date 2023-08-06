2fbcbab89d09607f436217e440d45291ac17fe84
GiesserCode <121457691+GiesserCode@users.noreply.github.com>
2023-08-06 17:05:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -121,3 +121,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("hashchange", scrollToHashPosition);
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } //else {
+        //entry.target.classList.remove('show');
+        //}
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
