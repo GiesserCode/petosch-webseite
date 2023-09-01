00accfec51b624a108268d21dad4fbbb3b514ce6
GiesserCode <121457691+GiesserCode@users.noreply.github.com>
2023-09-01 22:15:46 +0200

    Add files via upload

--- carousel.js
+++ carousel.js
@@ -16,3 +16,36 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel");
+    const slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+    let touchStartX = null;
+
+    carousel.addEventListener("touchstart", (e) => {
+        touchStartX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener("touchmove", (e) => {
+        if (touchStartX === null) return;
+        const touchEndX = e.touches[0].clientX;
+        const deltaX = touchStartX - touchEndX;
+
+        if (deltaX > 30) {
+            // Swipe left, go to the next slide
+            showSlide(currentIndex + 1);
+        } else if (deltaX < -30) {
+            // Swipe right, go to the previous slide
+            showSlide(currentIndex - 1);
+        }
+
+        touchStartX = null;
+    });
+
+    function showSlide(index) {
+        slides[currentIndex].removeAttribute("data-active");
+        currentIndex = (index + slides.length) % slides.length;
+        slides[currentIndex].setAttribute("data-active", "");
+    }
+});
