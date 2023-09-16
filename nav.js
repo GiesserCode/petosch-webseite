d7b751d24a994e308908fc77d5171b403277eb18
GiesserCode <121457691+GiesserCode@users.noreply.github.com>
2023-09-16 14:02:55 +0200

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,1 +1,105 @@
-document.addEventListener("DOMContentLoaded",function(){let e=document.querySelectorAll(".nav-item");document.querySelectorAll(".nav-link");let s=document.querySelectorAll(".dropdown-menu"),t=document.querySelector(".navbar-nav"),i=document.getElementById("menu-checkbox"),l=document.querySelector(".menu-toggle"),n=!1;function c(){s.forEach(e=>{e.style.display="none",e.classList.remove("slide-in")}),e.forEach(e=>{e.classList.remove("active")})}e.forEach(e=>{e.addEventListener("click",function(){if(this.classList.contains("active"))c(),window.innerWidth<1200&&(t.classList.remove("slide-in"),t.classList.add("slide-out"),setTimeout(()=>{t.style.display="none",t.classList.remove("slide-out"),c()},300),i.checked=!1,n=!1);else{c(),this.classList.add("active");let e=this.querySelector(".dropdown-menu");e.style.display="flex",e.classList.add("slide-in")}})});document.querySelector(".nav-menu-small").addEventListener("click",function(){n?(t.classList.remove("slide-in"),t.classList.add("slide-out"),setTimeout(()=>{t.style.display="none",t.classList.remove("slide-out"),c()},300),n=!1):(t.classList.remove("slide-out"),t.classList.add("slide-in"),t.style.display="block",c(),n=!0),i.checked=n}),i.addEventListener("change",function(){l.classList.toggle("checked",this.checked),(n=this.checked)||c()}),l.style.pointerEvents="none",window.addEventListener("resize",function(){window.innerWidth>1200?(t.style.display="flex",n=!0):(t.style.display="none",n=!1,c())})});
+document.addEventListener("DOMContentLoaded", function () {
+
+
+
+    const navItems = document.querySelectorAll(".nav-item");
+    const navLinks = document.querySelectorAll(".nav-link");
+    const dropdownMenus = document.querySelectorAll(".dropdown-menu");
+    const navbarNav = document.querySelector(".navbar-nav");
+    const menuCheckbox = document.getElementById("menu-checkbox");
+    const menuToggle = document.querySelector(".menu-toggle");
+    let isNavbarVisible = false; // Track the visibility state
+
+    // Function to hide all dropdown menus
+    function hideAllDropdownMenus() {
+        dropdownMenus.forEach((menu) => {
+            menu.style.display = "none";
+            menu.classList.remove("slide-in");
+        });
+        navItems.forEach((item) => {
+            item.classList.remove("active");
+        });
+    }
+
+    // ... (other parts of your code)
+
+    navItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            const isActive = this.classList.contains("active");
+            if (isActive) {
+                hideAllDropdownMenus();
+                if (window.innerWidth < 1200) {
+                    navbarNav.classList.remove("slide-in"); // Apply slide-out animation
+                    navbarNav.classList.add("slide-out");
+                    setTimeout(() => {
+                        navbarNav.style.display = "none";
+                        navbarNav.classList.remove("slide-out");
+                        hideAllDropdownMenus();
+                    }, 300);
+                    menuCheckbox.checked = false;
+                    isNavbarVisible = false;
+                }
+            } else {
+                hideAllDropdownMenus();
+                this.classList.add("active");
+                const dropdownMenu = this.querySelector(".dropdown-menu");
+                dropdownMenu.style.display = "flex";
+                dropdownMenu.classList.add("slide-in");
+            }
+        });
+    });
+
+    const navMenuSmall = document.querySelector(".nav-menu-small");
+    //const navbarNav = document.querySelector(".navbar-nav");
+    //const menuCheckbox = document.getElementById("menu-checkbox");
+    //const menuToggle = document.querySelector(".menu-toggle");
+    //let isNavbarVisible = false; // Track the visibility state
+
+    navMenuSmall.addEventListener("click", function () {
+        if (isNavbarVisible) {
+            navbarNav.classList.remove("slide-in");
+            navbarNav.classList.add("slide-out");
+            setTimeout(() => {
+                navbarNav.style.display = "none";
+                navbarNav.classList.remove("slide-out");
+                hideAllDropdownMenus(); // Hide dropdown menus when navbar is closed
+            }, 300);
+            isNavbarVisible = false;
+        } else {
+            navbarNav.classList.remove("slide-out");
+            navbarNav.classList.add("slide-in");
+            navbarNav.style.display = "block";
+            hideAllDropdownMenus(); // Hide dropdown menus when navbar is opened
+            isNavbarVisible = true;
+        }
+        menuCheckbox.checked = isNavbarVisible;
+    });
+
+    // Listen for checkbox change event
+    menuCheckbox.addEventListener("change", function () {
+        menuToggle.classList.toggle("checked", this.checked);
+        isNavbarVisible = this.checked;
+        if (!isNavbarVisible) {
+            hideAllDropdownMenus(); // Hide dropdown menus when navbar is closed by unchecking checkbox
+        }
+    });
+
+    // Disable pointer events for the label
+    menuToggle.style.pointerEvents = "none";
+
+    // Listen for window resize event
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 1200) {
+            navbarNav.style.display = "flex";
+            isNavbarVisible = true;
+        } else {
+            navbarNav.style.display = "none";
+            isNavbarVisible = false;
+            hideAllDropdownMenus(); // Hide dropdown menus when screen is less than 1200px
+        }
+    });
+
+});
+
+
+
